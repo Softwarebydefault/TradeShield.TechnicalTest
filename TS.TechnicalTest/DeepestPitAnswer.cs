ca5ac0404947bcf215ecd506b0e1c6aa5d7ae37d
Dewald Esterhuizen <dewald.esterhuizen@mintgroup.net>
2024-07-14 16:30:53 +0200

    Refactor to a for loop, fix index out of bound exception

--- TS.TechnicalTest/DeepestPitAnswer.cs
+++ TS.TechnicalTest/DeepestPitAnswer.cs
@@ -26,15 +26,10 @@ public class DeepestPitAnswer
         var pointsCount = points.Length;
         var deepestPitDepth = -1;
 
-        int leftIndex = 0;
         //Iterate through points
-        //In the problem statement leftIndex is referenced as P
-        while (leftIndex < pointsCount - 2)
+        for (int leftIndex = 0; leftIndex < pointsCount - 2; leftIndex++)  //In the problem statement leftIndex is referenced as P
         {
-            //Skip negative points
-            // Find the start of the pit (P)
-            while (leftIndex < pointsCount - 1 && points[leftIndex] <= 0)
-                leftIndex++;
+            if (points[leftIndex] <= 0) continue; // Skip non-positive points
 
             int rockBottomIndex = leftIndex + 1;
             //Assume a pit bottom and check if the next point has a decrease in slope, continue checking, 
@@ -43,18 +38,13 @@ public class DeepestPitAnswer
             while (rockBottomIndex < pointsCount - 1 && points[rockBottomIndex] > points[rockBottomIndex + 1])
                 rockBottomIndex++;
 
-            //Check if a bit bottom was found
-            if (rockBottomIndex == leftIndex + 1 || rockBottomIndex == pointsCount - 1)
-            {
-                leftIndex = rockBottomIndex;
-                continue;
-            }
+            if (rockBottomIndex == pointsCount - 1) continue; // No valid pit if at the end
 
             int rightIndex = rockBottomIndex + 1;
             //Assume the top of the pit as pit bottom, check the next point for an increase in slope,
             //With each successful check a new ridge should be tagged
             //In the problem statement rightIndex is referenced as R
-            while (rightIndex < pointsCount - 1 && points[rightIndex] < points[rightIndex + 1])
+            while (rightIndex + 1 < pointsCount && points[rightIndex] < points[rightIndex + 1])
                 rightIndex++;
 
             //Check if a ridge was found
@@ -65,8 +55,8 @@ public class DeepestPitAnswer
                 deepestPitDepth = Math.Max(deepestPitDepth, depth);
             }
 
-            // Move P to R and continue
-            leftIndex = rightIndex;
+            // Move leftIndex to the last rightIndex to avoid rechecking
+            leftIndex = rightIndex - 1;
         }
 
         return deepestPitDepth;
