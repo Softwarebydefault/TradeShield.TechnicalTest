d44bfd2efe111b32a35d4e6e21208c10645c5127
Dewald Esterhuizen <dewald.esterhuizen@mintgroup.net>
2024-07-14 15:57:48 +0200

    Question 2 - initial planning/approach

--- TS.TechnicalTest/DeepestPitAnswer.cs
+++ TS.TechnicalTest/DeepestPitAnswer.cs
@@ -2,8 +2,46 @@
 
 public class DeepestPitAnswer
 {
+  /*
+* 8
+* 7
+* 6
+* 5
+* 4    #                            #
+* 3   #  #                         #
+* 2  #    #        #              #
+* 1 #      #      # #            #
+--0---------#----#---#----------#----------------------
+* 1          #  #     #        #
+* 2           #        #      #
+* 3                     #    #
+* 4                      #  #
+* 5                       #
+* 6
+* 7
+* 8
+*/
     public static int Solution(int[] points)
     {
-        throw new NotImplementedException("Not completed yet");
+        var deepestPitDepth = -1;
+
+        //Iterate through points
+
+        //Skip negative points
+
+        //Assume a pit bottom and check if the next point has a decrease in slope, continue checking, 
+        //with each successful check a new pit bottom should be tagged
+
+        //Check if a bit bottom was found
+
+        //Assume the top of the pit as pit bottom, check the next point for an increase in slope,
+        //With each successful check a new ridge should be tagged
+
+        //Check if a ridge was found
+
+        //Calculate pit depth
+
+        return deepestPitDepth;
+
     }
 }
