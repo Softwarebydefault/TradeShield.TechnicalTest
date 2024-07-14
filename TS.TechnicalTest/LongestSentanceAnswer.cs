9ec61fc8642f0d470ba10a8ea3a9280b7f6ad7a6
Dewald Esterhuizen <dewald.esterhuizen@mintgroup.net>
2024-07-14 15:28:48 +0200

    Initial approach/plan

--- TS.TechnicalTest/LongestSentanceAnswer.cs
+++ TS.TechnicalTest/LongestSentanceAnswer.cs
@@ -5,6 +5,16 @@ public class LongestSentanceAnswer
 {
     public static int Solution(string s)
     {
-        throw new NotImplementedException("Not completed yet");
+        var sentenceLenght = 0;
+
+        //split string sentences based on dot
+
+        //Strip each sentence of punctuation marks
+
+        //split each sentence by spaces to get word count
+
+        //Determine sentence with highest word count
+
+        return sentenceLenght;
     }
 }
